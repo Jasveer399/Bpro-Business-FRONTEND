b414d4d57fb318642eda54cb3bf49004a088682f
Khushpreet Singh <khushramgaria@gmail.com>
2024-11-06 21:51:42 +0530

    change srver

--- src/Utils/server.js
+++ src/Utils/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:3000"
-// export const server = "https://bpro-business-backend.onrender.com"
+// export const server = "http://localhost:3000"
+export const server = "https://bpro-business-backend.onrender.com"
 
 //  ALl Dealers Route
 export const createDealerAccount = `${server}/api/v1/dealer/createDealerAccount`
